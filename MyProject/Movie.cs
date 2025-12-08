5e8af69a53c5098f66a6267694c9b457b04d762c
Stanislav Yunakov - STUDENT <G00471112@atu.ie>
2025-12-08 21:29:06 +0000

    Add properties for title, year, genres, director, rating, emoji, favourite, and viewed timestamp

--- MyProject/Movie.cs
+++ MyProject/Movie.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace MyProject
 {
-    internal class Movie
+    public class Movie
     {
+        // The movie's title 
+        public string Title { get; set; }
+
+        // Release year of the movie 
+        public int Year { get; set; }
+
+        // Array of genres 
+        public string[] Genres { get; set; }
+
+        // Director's name 
+        public string Director { get; set; }
+
+        // IMDB rating as a number 
+        public double IMDBRating { get; set; }
+
+        // Emoji representing the primary genre 
+        public string Emoji { get; set; }
+
+        // Whether the user has marked this movie as a favourite
+        public bool IsFavourite { get; set; }
+
+        // When the movie was last viewed 
+        public DateTime? ViewedAt { get; set; }
     }
 }
