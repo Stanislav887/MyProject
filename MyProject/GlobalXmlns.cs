0bd7917b0e47635bd009a0f23567034ca4410598
MSI <MSI@DESKTOP-7ENG9EF>
2025-12-08 20:38:40 +0000

    Добавьте файлы проекта.

---
+++ MyProject/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MyProject")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MyProject.Pages")]
