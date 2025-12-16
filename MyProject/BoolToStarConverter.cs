fba95d38aa3dea6d3c41d1fd7efb1ee2316394fa
Stanislav Yunakov - STUDENT <G00471112@atu.ie>
2025-12-16 22:29:44 +0000

    Create BoolToStarConverter class for favorite star display

---
+++ MyProject/BoolToStarConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.Maui.Controls;
+
+namespace MyProject
+{
+    public class BoolToStarConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is bool isFavorite && isFavorite ? "★" : "☆";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
