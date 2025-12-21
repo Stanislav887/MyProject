b7a826f69170b52f27bdcc649b0a6e9a97f46001
Stanislav Yunakov - STUDENT <G00471112@atu.ie>
2025-12-21 20:08:24 +0000

    Add MovieHistoryEntry model for tracking user activity

---
+++ MyProject/MovieHistoryEntry.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProject
+{
+    public class MovieHistoryEntry
+    {
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public string Genre { get; set; }
+        public string Emoji { get; set; }
+
+        // "Viewed", "Favorited", "Unfavorited"
+        public string Action { get; set; }
+
+        public DateTime Timestamp { get; set; }
+    }
+}
