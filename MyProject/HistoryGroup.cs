05d2f793ece4d0af5c36ff121eefe35247911062
Stanislav Yunakov - STUDENT <G00471112@atu.ie>
2025-12-23 19:28:18 +0000

    Add HistoryGroup class to support grouping history by date

---
+++ MyProject/HistoryGroup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace MyProject
+{
+    public class HistoryGroup : ObservableCollection<MovieHistoryEntry>
+    {
+        public string Date { get; private set; }
+
+        public HistoryGroup(string date, ObservableCollection<MovieHistoryEntry> entries) : base(entries)
+        {
+            Date = date;
+        }
+    }
+}
