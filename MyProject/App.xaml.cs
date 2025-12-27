c58a5c541e7f7c23473a3e531fca15cc8d6470ff
Stanislav Yunakov - STUDENT <G00471112@atu.ie>
2025-12-27 23:22:08 +0000

    Apply saved app theme on startup in App constructor

--- MyProject/App.xaml.cs
+++ MyProject/App.xaml.cs
@@ -5,6 +5,21 @@
         public App()
         {
             InitializeComponent();
+
+            // Apply saved theme
+            string savedTheme = Preferences.Default.Get("AppTheme", "System Default");
+            switch (savedTheme)
+            {
+                case "Light":
+                    UserAppTheme = AppTheme.Light;
+                    break;
+                case "Dark":
+                    UserAppTheme = AppTheme.Dark;
+                    break;
+                default:
+                    UserAppTheme = AppTheme.Unspecified;
+                    break;
+            }
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
