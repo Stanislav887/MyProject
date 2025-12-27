7a050e0f03c9a089498452df2a8dd2a55cca5180
Stanislav Yunakov - STUDENT <G00471112@atu.ie>
2025-12-27 22:58:42 +0000

    Add theme picker handler to save and apply selected app theme immediately

--- MyProject/SettingsPage.xaml.cs
+++ MyProject/SettingsPage.xaml.cs
@@ -12,4 +12,25 @@ public partial class SettingsPage : ContentPage
         string theme = Preferences.Default.Get("AppTheme", "System Default");
         ThemePicker.SelectedIndex = ThemePicker.Items.IndexOf(theme);
     }
+
+    private void ThemePicker_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        string selected = ThemePicker.SelectedItem.ToString();
+        Preferences.Default.Set("AppTheme", selected);
+
+        // Apply theme immediately
+        switch (selected)
+        {
+            case "Light":
+                Application.Current.UserAppTheme = AppTheme.Light;
+                break;
+            case "Dark":
+                Application.Current.UserAppTheme = AppTheme.Dark;
+                break;
+            default:
+                Application.Current.UserAppTheme = AppTheme.Unspecified;
+                break;
+        }
+    }
+
 }
