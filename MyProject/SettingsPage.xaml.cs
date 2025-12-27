e3251093f3efbf0b6450a7e665e1758766a61650
Stanislav Yunakov - STUDENT <G00471112@atu.ie>
2025-12-27 22:51:09 +0000

    Load saved theme preference on SettingsPage

--- MyProject/SettingsPage.xaml.cs
+++ MyProject/SettingsPage.xaml.cs
@@ -1,3 +1,5 @@
+using Microsoft.Maui.Controls;
+
 namespace MyProject;
 
 public partial class SettingsPage : ContentPage
@@ -5,5 +7,9 @@ public partial class SettingsPage : ContentPage
 	public SettingsPage()
 	{
 		InitializeComponent();
-	}
+
+        // Load saved theme from Preferences
+        string theme = Preferences.Default.Get("AppTheme", "System Default");
+        ThemePicker.SelectedIndex = ThemePicker.Items.IndexOf(theme);
+    }
 }
