e05995766bd558f71bea1e22895197e5542c084e
Stanislav Yunakov - STUDENT <G00471112@atu.ie>
2026-01-06 22:07:20 +0000

    Add TapGestureRecognizer to movie Frame for mobile taps

--- MyProject/MainPage.xaml.cs
+++ MyProject/MainPage.xaml.cs
@@ -22,6 +22,7 @@ namespace MyProject
         }
 
 
+
         private void SearchBar_TextChanged(object sender, TextChangedEventArgs e)
         {
             string searchText = e.NewTextValue ?? "";
